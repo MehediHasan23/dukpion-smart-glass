02a8b188da736acf6566e50079df12456faed633
MehediHasan23 <mehedi12307@gmail.com>
2021-11-10 13:10:46 -0800

    fixed some data

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import Banner from '../../Shared/Banner/Banner';
-import Products from '../Products/Products';
+import React from "react";
+import Banner from "../../Shared/Banner/Banner";
+import Products from "../Products/Products";
 
 const Home = () => {
   return (
@@ -13,104 +13,117 @@ const Home = () => {
 
 export default Home;
 
-
-   /*  const glasses = [
-   {
-     name: "Black Sparkle Eyeglass",
-     price: "720",
-     img: "https://img-for-json.netlify.app/images/1.png",
-     rating: "4.5",
-     category: "Rectangle",
-   },
-   {
-     name: "Full Transparent Eyeglass",
-     price: "1800",
-     img: "https://img-for-json.netlify.app/images/2.jpg",
-     rating: "4.3",
-     category: "Rectangle",
-   },
-   {
-     name: "Semi Oval Ass Men Frame",
-     price: "3599",
-     img: "https://img-for-json.netlify.app/images/3.jpg",
-     rating: "4.5",
-     category: "Oval",
-   },
-   {
-     name: "All Time Hit Fashion Eyeware",
-     price: "2550",
-     img: "https://img-for-json.netlify.app/images/4.png",
-     rating: "3.5",
-     category: "Oval",
-   },
-   {
-     name: "Cat Eye Eyeglass For Lady",
-     price: "1656",
-     img: "https://img-for-json.netlify.app/images/5.png",
-     rating: "4.5",
-     category: "CATEYE",
-   },
-   {
-     name: "Transparent Women Eyeglass ",
-     price: "834",
-     img: "https://img-for-json.netlify.app/images/6.png",
-     rating: "5",
-     category: "CATEYE",
-   },
-   {
-     name: "FASHION Sunglass ",
-     price: "634",
-     img: "https://img-for-json.netlify.app/images/7.png",
-     rating: "3.5",
-     category: "GRADIENT",
-   },
-   {
-     name: "Stylish Yellow Sunglass ",
-     price: "740",
-     img: "https://img-for-json.netlify.app/images/8.png",
-     rating: "4.5",
-     category: "POLARIZED",
-   },
-   {
-     name: "Coolest SunGlasses ",
-     price: "637",
-     img: "https://img-for-json.netlify.app/images/9.png",
-     rating: "5",
-     category: "POLARIZED",
-   },
-   {
-     name: "Pilot Shade Men's Sunglass ",
-     price: "1737",
-     img: "https://img-for-json.netlify.app/images/10.png",
-     rating: "4.2",
-     category: "PILOT",
-   },
-   {
-     name: "Pilot Shade Men's Sunglass ",
-     price: "1737",
-     img: "https://img-for-json.netlify.app/images/11.png",
-     rating: "4.2",
-     category: "PILOT",
-   },
-   {
-     name: "Premium Pilot Sunglass ",
-     price: "2937",
-     img: "https://img-for-json.netlify.app/images/12.png",
-     rating: "4.8",
-     category: "PILOT",
-   },
-   {
-     name: "Retro Polarized Sunglasses ",
-     price: "2550",
-     img: "https://img-for-json.netlify.app/images/13.png",
-     rating: "4.5",
-     category: "PREMIUM",
-   },
-   {
-     name: "Clubmaster Lady Sunglass ",
-     price: "2937",
-     img: "https://img-for-json.netlify.app/images/14.png",
-     rating: "5",
-     category: "CLUBMASTER",
-   },
- ];     */ 
+/* const glasses = [
+  {
+    name: "Black Sparkle Eyeglass",
+    price: "720",
+    img: "https://img-for-json.netlify.app/images/1.png",
+    rating: "4.5",
+    ratingCount: 509,
+    category: "Rectangle",
+  },
+  {
+    name: "Full Transparent Eyeglass",
+    price: "1800",
+    img: "https://img-for-json.netlify.app/images/2.jpg",
+    rating: "4.3",
+    ratingCount: 280,
+    category: "Rectangle",
+  },
+  {
+    name: "Semi Oval Ass Men Frame",
+    price: "3599",
+    img: "https://img-for-json.netlify.app/images/3.jpg",
+    rating: "4.5",
+    ratingCount: 1033,
+    category: "Oval",
+  },
+  {
+    name: "All Time Hit Fashion Eyeware",
+    price: "2550",
+    img: "https://img-for-json.netlify.app/images/4.png",
+    rating: "3.5",
+    ratingCount: 1209,
+    category: "Oval",
+  },
+  {
+    name: "Cat Eye Eyeglass For Lady",
+    price: "1656",
+    img: "https://img-for-json.netlify.app/images/5.png",
+    rating: "4.5",
+    ratingCount: 3100,
+    category: "CATEYE",
+  },
+  {
+    name: "Transparent Women Eyeglass ",
+    price: "834",
+    img: "https://img-for-json.netlify.app/images/6.png",
+    rating: "5",
+    ratingCount: 970,
+    category: "CATEYE",
+  },
+  {
+    name: "FASHION Sunglass ",
+    price: "634",
+    img: "https://img-for-json.netlify.app/images/7.png",
+    rating: "3.5",
+    ratingCount: 780,
+    category: "GRADIENT",
+  },
+  {
+    name: "Stylish Yellow Sunglass ",
+    price: "740",
+    img: "https://img-for-json.netlify.app/images/8.png",
+    rating: "4.5",
+    ratingCount: 990,
+    category: "POLARIZED",
+  },
+  {
+    name: "Coolest SunGlasses ",
+    price: "637",
+    img: "https://img-for-json.netlify.app/images/9.png",
+    rating: "5",
+    ratingCount: 1350,
+    category: "POLARIZED",
+  },
+  {
+    name: "Pilot Shade Men's Sunglass ",
+    price: "1737",
+    img: "https://img-for-json.netlify.app/images/10.png",
+    rating: "4.2",
+    ratingCount: 1580,
+    category: "PILOT",
+  },
+  {
+    name: "Pilot Shade Men's Sunglass ",
+    price: "1737",
+    img: "https://img-for-json.netlify.app/images/11.png",
+    rating: "4.2",
+    ratingCount: 770,
+    category: "PILOT",
+  },
+  {
+    name: "Premium Pilot Sunglass ",
+    price: "2937",
+    img: "https://img-for-json.netlify.app/images/12.png",
+    rating: "4.8",
+    ratingCount: 990,
+    category: "PILOT",
+  },
+  {
+    name: "Retro Polarized Sunglasses ",
+    price: "2550",
+    img: "https://img-for-json.netlify.app/images/13.png",
+    rating: "4.5",
+    ratingCount: 1190,
+    category: "PREMIUM",
+  },
+  {
+    name: "Clubmaster Lady Sunglass ",
+    price: "2937",
+    img: "https://img-for-json.netlify.app/images/14.png",
+    rating: "5",
+    ratingCount: 506,
+    category: "CLUBMASTER",
+  },
+]; */
--- src/Pages/Home/Product/Product.js
+++ src/Pages/Home/Product/Product.js
@@ -8,29 +8,38 @@ import { Link } from "react-router-dom";
 import Rating from "react-rating";
 const Product = (props) => {
   console.log(props);
-  const { name, img, rating, category, price,  _id } = props.product;
+  const { name, img, rating, ratingCount, category, price, _id } =
+    props.product;
   return (
     <div className="col-sm-12 col-md-6 col-lg-4">
       <div
         className="card mx-auto className='mb-0' border m-2"
         style={{ width: "18rem" }}
       >
-        <img  src={img} className="card-img-top img-fluid " alt="..." />
-        <div className="card-body text-center m-0">
+        <img src={img} className="card-img-top img-fluid " alt="..." />
+        <div className="card-body text-start m-0">
           <h5 className="card-title">{name}</h5>
           <div className="text-start">
             <Rating
               readonly
-              className="text-warning"
+              className="text-danger"
               initialRating={rating}
               emptySymbol={<FontAwesomeIcon icon={emptyStar} />}
               fullSymbol={<FontAwesomeIcon icon={fullStar} />}
             />{" "}
             <span className="text-muted">{rating}</span>
+            <span className="text-muted">( reviews {ratingCount})</span>
           </div>
-          <p>Price: {price}</p>
+          <div className="text-danger mt-3">
+            <span className="fw-bold">
+              <sup className="fs-6">$</sup> {price}
+            </span>
+          </div>
+          <p className='fw-bold'>SIZE: {category}</p>
           <Link to={`/order/${_id}`}>
-            <Button className="btn btn-primary w-100 fw-bold">Add to Cart</Button>
+            <Button className="btn btn-primary w-100 fw-bold">
+              Add to Cart
+            </Button>
           </Link>
         </div>
       </div>
